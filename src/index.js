a58eca58735b82dc32efd690d5e74cf82df92f07
Ali Sajid <imalisajid@gmail.com>
2018-11-14 17:51:39 +0500

    add_app_module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import App from '../src/App'
 
-class HelloMessage extends React.Component {
-  render() {
-    return <div>Hello { this.props.name }</div>;
-  }
-}
-
-var mountNode = document.getElementById('root');
-ReactDOM.render(<HelloMessage name='Ali'/>, mountNode);
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ const config = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: 'babel-loader',
+
       },
     ],
   },
