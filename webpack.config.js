199e708009bb0ae2d9ee40cb0d30a1eb8e8365b7
Ali Sajid <imalisajid@gmail.com>
2018-11-15 19:23:29 +0500

    added_loaders_for_assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,66 @@
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: ['babel-loader']
-			},
-			{
-				test: /\.css$/,
-				use: [ 'style-loader', 'css-loader' ]
-			},
-			{
-				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-				loader: "file-loader"
-			},
-			{
-				test: /\.svg$/,
-				use: [
-					{
-						loader: "babel-loader"
-					},
-					{
-						loader: "react-svg-loader",
-						options: {
-							jsx: true // true outputs JSX tags
-						}
-					}
-				]
-			}
-		]
-	},
-	resolve: {
-		extensions: ['*', '.js', '.jsx']
-	},
-	output: {
-		path: __dirname + '/dist',
-		publicPath: '/',
-		filename: 'bundle.js'
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	],
-	devServer: {
-		contentBase: './dist',
-		hot: true
-	}
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: '[name].js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'image/svg+xml',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|ico)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
