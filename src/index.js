2320ba3e4d9dd2284d4b101f4bf001372e78f17d
Ali Sajid <imalisajid@gmail.com>
2018-11-15 14:18:39 +0500

    removed_commented_code

--- src/index.js
+++ src/index.js
@@ -3,16 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-module.hot.accept();
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-const title = 'My Minimal React Webpack Babel Setup';
-
-ReactDOM.render(
-	<div>{title}</div>,
-	document.getElementById('app')
-);
 module.hot.accept();
-*/
