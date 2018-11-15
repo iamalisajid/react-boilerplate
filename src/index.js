13f3cf6cddb89f0dedc9fd8bb3490ac35fe58793
Ali Sajid <imalisajid@gmail.com>
2018-11-15 14:08:22 +0500

    initial_app_setup

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+module.hot.accept();
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+	<div>{title}</div>,
+	document.getElementById('app')
+);
+module.hot.accept();
+*/
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,51 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-
-const htmlWebpackPlugin = new HtmlWebPackPlugin({
-  template: './public/index.html',
-  filename: './index.html',
-});
+const webpack = require('webpack');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: '[name]_[local]_[hash:base64]',
-              sourceMap: true,
-              minimize: true,
-            },
-          },
-          {
-            test: /\.(gif|svg|jpg|png)$/,
-            loader: 'file-loader',
-          },
-          {
-            test: /\.svg$/,
-            loader: 'svg-inline-loader',
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [htmlWebpackPlugin],
+	entry: './src/index.js',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ]
+			},
+			{
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "file-loader"
+			},
+			{
+				test: /\.svg$/,
+				use: [
+					{
+						loader: "babel-loader"
+					},
+					{
+						loader: "react-svg-loader",
+						options: {
+							jsx: true // true outputs JSX tags
+						}
+					}
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx']
+	},
+	output: {
+		path: __dirname + '/dist',
+		publicPath: '/',
+		filename: 'bundle.js'
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	],
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	}
 };
