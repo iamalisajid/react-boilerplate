f134d8279c97735f36d316e1c50299336971ad5f
Ali Sajid <imalisajid@gmail.com>
2018-11-16 17:10:04 +0500

    merged_contacts_todo_app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import App from './viewes/App';
+import { getStore, persistor } from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <Provider store={getStore}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);
 module.hot.accept();
